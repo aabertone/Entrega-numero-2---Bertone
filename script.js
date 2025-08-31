69f30dca2c73b25dd9b54e9bcad9a24d3e5146bc
Ariel <aabertone@hotmail.com>
2025-08-31 08:54:17 -0300

    Version inicial

---
+++ script.js
@@ -0,0 +1,160 @@
+// funcion para recargar si el usuario cancela antes de empezar
+function reloadpagina() {
+    alert("Operacion cancelada, gracias por participar");
+    location.reload();
+}
+// funcion para recargar al finalizar el juego
+function findeljuego() {
+    location.reload();
+}
+
+// solicitud de datos jugador 1 devuelve una matriz con datos
+function datosjugador1() {
+let nombre1 = prompt("Jugador 1 ingrese su nombre");
+if (nombre1 === null) {
+    reloadpagina();
+  }
+while (!nombre1 || !isNaN(nombre1)) {
+  alert("Debe ingresar un nombre");
+  nombre1 = prompt("Jugador 1 ingrese su nombre");
+  if (nombre1 === null) {
+    reloadpagina();
+  }
+}
+let edad1 = prompt("Bienvenido/a " + nombre1 + " ingrese su edad");
+if (edad1 === null) {
+    reloadpagina();
+  }
+while (isNaN(edad1) || edad1 < 18) {
+  alert("Debes ser mayor de edad para jugar.");
+  edad1 = prompt(nombre1 + " ingrese su edad");
+  if (edad1 === null) {
+    reloadpagina();
+  }
+}
+let apuesta1 = prompt(nombre1 + " ingrese el monto de su apuesta");
+if (apuesta1 === null) {
+    reloadpagina();
+  }
+while (isNaN(apuesta1) || apuesta1 < 10 || apuesta1 > 100) {
+  alert("La apuesta debe ser un numero entre 10$ y 100$");
+  apuesta1 = prompt(nombre1 + " ingrese el monto de su apuesta");
+  if (apuesta1 === null) {
+    reloadpagina();
+  }
+}
+let numero1 = prompt(nombre1 + " ingrese su numero a adivinar recuerde pedirle a su compañero que no mire");
+if (numero1 === null) {
+    reloadpagina();
+  }
+while (isNaN(numero1)) {
+    alert("Debe ingresar un numero");
+    numero1 = prompt(nombre1 + " ingrese su numero a adivinar recuerde pedirle a su compañero que no mire");
+    if (numero1 === null) {
+    reloadpagina();
+  }
+}
+return [nombre1 , apuesta1 , numero1 ];
+}
+
+// solicitud de datos jugador 2 devuelve una matriz con datos
+function datosjugador2() {   
+let nombre2 = prompt("Jugador 2 ingrese su nombre");
+if (nombre2 === null) {
+    reloadpagina();
+  }
+while (!nombre2 || !isNaN(nombre2)) {
+  alert("Debe ingresar un nombre");
+  nombre2 = prompt("Jugador 2 ingrese su nombre");
+  if (nombre2 === null) {
+    reloadpagina();
+  }
+}
+let edad2 = prompt("Bienvenido/a " + nombre2 + " ingrese su edad");
+if (edad2 === null) {
+    reloadpagina();
+  }
+while (isNaN(edad2) || edad2 < 18) {
+  alert("Debes ser mayor de edad para jugar.");
+  edad2 = prompt(nombre2 + " ingrese su edad");
+    if (edad2 === null) {
+    reloadpagina();
+  }
+}
+let apuesta2 = prompt(nombre2 + " ingrese el monto de su apuesta");
+if (apuesta2 === null) {
+    reloadpagina();
+  }
+while (isNaN(apuesta2) || apuesta2 < 10 || apuesta2 > 100) {
+  alert("La apuesta debe ser un numero entre 10$ y 100$");
+  apuesta2 = prompt(nombre2 + " ingrese el monto de su apuesta");
+    if (apuesta2 === null) {
+    reloadpagina();
+  }
+}
+let numero2 = prompt(nombre2 + " ingrese su numero a adivinar recuerde pedirle a su compañero que no mire");
+if (numero2 === null) {
+    reloadpagina();
+  }
+while (isNaN(numero2)) {
+    alert("Debe ingresar un numero");
+    numero2 = prompt(nombre2 + " ingrese su numero a adivinar recuerde pedirle a su compañero que no mire");
+    if (numero2 === null) {
+    reloadpagina();
+  }
+}
+return [nombre2 , apuesta2 , numero2 ];
+}
+
+// funcion principal del juego, evalua, descuenta y loopea hasta que alguien gane o pierda o se retire.
+function juego() {
+    alert("Bienvenido al juego\n\nEn este juego jugarán 2 jugadores, cada jugador deberá pagar para poder jugar un monto entre 10$ y 100$\n\nReglas del juego: Debes ser mayor de edad para poder jugar, cada jugador ingresará 1 numero que el otro jugador deberá adivinar, si el jugador adivina el numero de su compañero gana el juego y se le devolverá el dinero que le quede mas el dinero de su contricante, si no lo adivina pierde su turno\nPor cada turno que el jugador no adivine perderá 10$ de su apuesta inicial, ten en cuenta que si pierdes muchas veces tu premio se irá reduciendo\nEl juego termina cuando 1 jugador adivina o cuando algun jugador se queda sin dinero\n\nTe recordamos que no hay reembolsos, una vez esten registrados ambos jugadores y empice el juego si deseas retirarte se considera que pierdes y le daremos el dinero a tu contrincante\n\nSuerte a ambos jugadores");
+  let usuario1 = datosjugador1();
+  let usuario2 = datosjugador2();
+  while (usuario1[1] > 0 && usuario2[1] > 0) {
+  let adivinanza1 = prompt(usuario1[0] + " adivine el valor de su compañero");
+  if (adivinanza1 === null) {
+    alert(usuario1[0] + " has decidido retirarte, " + usuario2[0] + " aqui tienes tus $" + (Number(usuario1[1]) + Number(usuario2[1])));
+    location.reload();
+    return;
+  }
+  if (adivinanza1 == usuario2[2]) {
+    alert("Felicidades " + usuario1[0] + " has adivinado el numero de tu compañero y ganado $" + (Number(usuario1[1]) + Number(usuario2[1])));
+    break;
+  } else {
+    usuario1[1] = usuario1[1] - 10;
+    alert("Lo siento " + usuario1[0] + " no has adivinado el numero de tu compañero, perdiste $10 te quedan $" + usuario1[1]);
+  }
+  if (usuario1[1] <= 0) {
+    break;
+  }
+  let adivinanza2 = prompt(usuario2[0] + " adivine el valor de su compañero");
+  if (adivinanza2 === null) {
+    alert(usuario2[0] + " has decidido retirarte, " + usuario1[0] + " aqui tienes tus $" + (Number(usuario1[1]) + Number(usuario2[1])));
+    location.reload();
+    return;
+  }
+  if (adivinanza2 == usuario1[2]) {
+    alert("Felicidades " + usuario2[0] + " has adivinado el numero de tu compañero y ganado $" + (Number(usuario1[1]) + Number(usuario2[1])));
+    break;
+  } else {
+    usuario2[1] = usuario2[1] - 10;
+    alert("Lo siento " + usuario2[0] + " no has adivinado el numero de tu compañero, perdiste $10 te quedan $" + usuario2[1]);
+  }
+}
+if (usuario1[1] <= 0) {
+  alert("Lo siento " + usuario1[0] + " te has quedado sin dinero, perdiste el juego. " + usuario2[0] + " aqui tienes tus $" + usuario2[1]);
+}else if (usuario2[1] <= 0) {
+  alert("Lo siento " + usuario2[0] + " te has quedado sin dinero, perdiste el juego. " + usuario1[0] + " aqui tienes tus $" + usuario1[1]);
+}else {
+  alert("El juego ha terminado.");
+}
+}
+
+juego();
+findeljuego();
+
+
+
+
+
