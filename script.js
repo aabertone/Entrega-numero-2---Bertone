855bf0dd64db811ee5c2d756adcb4c95d8032c9a
Ariel <aabertone@hotmail.com>
2025-10-21 22:29:32 -0300

    Agregado de Sweet alert y contador

--- script.js
+++ script.js
@@ -28,9 +28,9 @@ let datosjugador1 = document.getElementById('form1')
 datosjugador1.innerHTML = `
 <form>
   <label for="nombre1">Nombre Jugador:</label>
-  <input type="text" id="nombre1" name="nombre1" required><br><br>
+  <input type="text" id="nombre1" name="nombre1"><br><br>
   <label for="apuesta">Apuesta: Entre $1 y 100$</label>
-  <input type="number" id="apuestavalor" name="apuesta" min="1" required><br><br>
+  <input type="number" id="apuestavalor" name="apuesta"><br><br>
   <input type="submit" value="Enviar" id="enviar1">
 </form>
 `;
@@ -44,11 +44,19 @@ jugar.addEventListener('click', function(){
     const nombre1 = document.getElementById('nombre1').value;
     let apuesta = document.getElementById('apuestavalor').value;
     if(!nombre1 || !isNaN(nombre1)){
-        alert("Debe ingresar un nombre");
+        Swal.fire({
+          icon: "warning",
+          title: "Intentalo de nuevo",
+          text: "Debes ingresar un nombre valido",
+          });
         return;
     }
     if(!apuesta || isNaN(apuesta) || apuesta <= 0 || apuesta > 100){
-      alert("Debe ingresar una apuesta valida");
+      Swal.fire({
+          icon: "warning",
+          title: "Intentalo de nuevo",
+          text: "Debes ingresar una apuesta valida",
+          });
       return;
     }
   jugar.disabled = true;
@@ -62,26 +70,56 @@ jugar.addEventListener('click', function(){
     adivinar.innerHTML = `
     <h2>Adivina el numero</h2>
     <p>Bienvenido ${nombre1} ingrese el numero</p>
-    <input type="number" id="adivina" name="adivina" min="0" max="10" required><br><br>
+    <input class="input" type="number" id="adivina" name="adivina" min="0" max="20" required><br><br>
     <input type="submit" value="Adivinar" id="botonadivina">
     <p>El numero a adivinar es entre 0 y 20</p>
+    <span>Tiempo:</span>
+    <span id="contador">60</span>
     `;
     mostrarapuesta(apuesta);
+    // Contador de tiempo
+    let contador = 60;
+    const contadorElemento = document.getElementById("contador");
+    function actualizarContador() {
+      contador--;
+      contadorElemento.textContent = contador;
+      if (contador <= 0) {
+    clearInterval(intervalo);
+    Swal.fire({
+          icon: "error",
+          title: "Se termino el tiempo lo siento",});
+    reiniciarjuego();
+    return;
+        }
+    }
+    const intervalo = setInterval(actualizarContador, 1000)
     // Logica del juego
     document.getElementById('botonadivina').addEventListener('click', function(){
       if(document.getElementById('adivina').value == numerorandom){
-        alert("Felicidades " + nombre1 + " has adivinado el numero ");
+        Swal.fire({
+          icon: "success",
+          text: "Felicidades " + nombre1 + " has adivinado el numero!",
+        });
+          clearInterval(intervalo);
           reiniciarjuego();
           return;
         }
         else{
           apuesta = apuesta - 10
-          alert("Lo siento " + nombre1 + " no has adivinado el numero, vuelve a intentarlo");}
+          Swal.fire({
+            icon: "error",
+            toast: true,
+            timer: 1200,
+            showConfirmButton: false,
+            position: "center-end",
+            text: "Lo siento " + nombre1 + " no has adivinado el numero, vuelve a intentarlo"});
+        }
           mostrarapuesta(apuesta);
 
           //eliminacion de datos y reinicio del juego si no hay dinero
           if(apuesta <= 0){
-            alert("Lo siento " + nombre1 + " te has quedado sin dinero, el juego terminado");
+            Swal.fire("Lo siento " + nombre1 + " te has quedado sin dinero, el juego terminado");
+            clearInterval(intervalo);
             reiniciarjuego();
             return;
           }
